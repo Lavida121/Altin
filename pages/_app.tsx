6d185b18909ac845c384b6ceced0ca8e697cd134
Lavida121 <lavidaloca2312@gmail.com>
2025-05-23 19:42:21 +0200

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,18 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { appWithTranslation } from 'next-i18next'
+import Head from 'next/head'   // <--- importiere Head hier
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        {/* Wichtig für mobiles korrektes Scaling */}
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
+
 export default appWithTranslation(MyApp)
